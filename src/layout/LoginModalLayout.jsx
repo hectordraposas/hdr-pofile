9c586b073329914e3b460287122cab275304aaa9
Hector Raposas <hector.raposas@gmail.com>
2024-05-04 17:03:38 +0800

    commit on modal

--- src/layout/LoginModalLayout.jsx
+++ src/layout/LoginModalLayout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import { IoMdCloseCircle } from "react-icons/io";
 const LoginModalLayout = ({ loginModal }) => {
   const handleOverLay = (e) => {
     if (e.currentTarget !== e.target) return;
@@ -7,11 +7,19 @@ const LoginModalLayout = ({ loginModal }) => {
   };
   return (
     <div
-      className="overlay w-full h-full backdrop-blur-md bg-slate-800 fixed inset-0 bg-opacity-20 z-50"
+      className="overlay w-full h-full backdrop-blur-md bg-slate-800 fixed inset-0 bg-opacity-20 z-50 transition-all"
       onClick={handleOverLay}
     >
+      <button className="float-right md:block hidden mr-9 mt-5 text-3xl text-red-700">
+        <IoMdCloseCircle onClick={handleOverLay} />
+      </button>
       <figure className="inset-0 md:h-fit md:w-1/2 lg:h-fit lg:w-1/2 xl:w-1/3 h-full w-full bg-slate-700 z-10  fixed mx-auto my-auto transition-all backdrop-blur-md p-10">
-        <h1 className="text-center text-slate-50 md:text-2xl">Please Login</h1>
+        <button className="float-right md:hidden block  text-red-700 text-2xl">
+          <IoMdCloseCircle onClick={handleOverLay} />
+        </button>
+        <h1 className="text-center text-slate-50 md:text-2xl mt-5">
+          Please Login
+        </h1>
         <div className="flex flex-col gap-2">
           <figure className="flex flex-col gap-2">
             <label htmlFor="uname" className="text-slate-50">
